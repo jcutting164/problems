e70dca158f8fa5e5ff5e7d0ffcf28c62a8f01973
Josh Cutting <jcutting164@gmail.com>
2018-02-08 06:48:59 -0500

    Create Items.java

---
+++ Game/src/Items.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Items {
+	
+	public String item;
+	public int item_value;
+	
+	public Items(){
+		Initialize init = new Initialize();
+		HashMap<String, Integer> weaponsMap = init.getWeaponMap(); 
+		HashMap<String, Integer> defenceMap = init.getDefenseMap();
+		ArrayList<String> weaponsList = init.getWeaponList();
+		ArrayList<String> defenceList = init.getDefenceList();
+		
+		int choice = init.getRandom(1, 2);
+		if(choice == 1){
+			int choice2 = init.getRandom(0, 9);
+			item = weaponsList.get(choice2);
+			item_value = weaponsMap.get(item);
+		}else if(choice ==2){
+			int choice2 = init.getRandom(0, 9);
+			item = defenceList.get(choice2);
+			item_value = defenceMap.get(item);
+		}
+		
+		
+		
+		
+	}
+	
+}
