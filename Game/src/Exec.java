2f32e7f1540f9910f5cfac49966179aa14707183
Josh Cutting <jcutting164@gmail.com>
2018-02-08 06:47:49 -0500

    Update Exec.java

--- Game/src/Exec.java
+++ Game/src/Exec.java
@@ -3,9 +3,24 @@ import java.util.ArrayList;
 
 public class Exec {
 	public static void main(String[] args){
-		ArrayList<String> inv = new ArrayList<String>();
-		Player p1 = new Player(10, 5, 5, inv);
-		Enemy e1 = new Enemy(10, 5, 5);
-		p1.Battle(p1, e1);
+		ArrayList<Items> inv = new ArrayList<Items>();
+		
+		Items first = new Items();
+		first.item = "Sword";
+		first.item_value = 1;
+		
+		Items second = new Items();
+		second.item = "Leather-Jacket";
+		second.item_value = 1;
+		
+		Player player1 = new Player(2, 0, 0, inv, first, second);
+
+		
+		
+		Enemy enemy1 = new Enemy();
+		
+		System.out.println(enemy1.size+ " "+enemy1.color+" "+enemy1.type);
+		System.out.println("Health: "+ enemy1.health_value);
+		player1.Battle(player1, enemy1);
 	}
 }
