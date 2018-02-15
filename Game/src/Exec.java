55cc38b64726e9a8ba9ed876f3c9fd822d099c96
Josh Cutting <jcutting164@gmail.com>
2018-02-15 15:17:13 -0500

    Update Exec.java

--- Game/src/Exec.java
+++ Game/src/Exec.java
@@ -14,13 +14,20 @@ public class Exec {
 		second.item_value = 1;
 		
 		Player player1 = new Player(1, 100, 5, inv, first, second);
-
-		
-		
-		Enemy enemy1 = new Enemy();
-		
-		System.out.println(enemy1.size+ " "+enemy1.color+" "+enemy1.type);
-		System.out.println("Health: "+ enemy1.health_value);
-		player1.Battle(player1, enemy1);
+		int round = 0;
+		Scanner scan = new Scanner();
+		while(player1.health_value > 0){
+			Enemy enemy1 = new Enemy();
+			System.out.println("Do you want to equip an item? (yes/no)");
+			String response = scan.next();
+			if(response.equals("yes")){
+				player1.equip(inv, player1);
+			}
+			System.out.println(enemy1.size+ " "+enemy1.color+" "+enemy1.type);
+			System.out.println("Health: "+ enemy1.health_value);
+			player1.Battle(player1, enemy1);
+			round++
+		}
+		System.out.println("You lasted "+round+" rounds");
 	}
 }
